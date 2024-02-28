0ca6daa1c37cd85dad83905a6178cfc4e4f278fd
Jaehui Yu <pkj0522@naver.com>
2024-02-28 10:45:27 +0900

    feat: Set up design system

---
+++ BitBox/DesignSystem/ColorStyle.swift
@@ -0,0 +1,21 @@
+//
+//  ColorStyle.swift
+//  BitBox
+//
+//  Created by Jaehui Yu on 2/28/24.
+//
+
+import UIKit
+
+enum ColorStyle {
+    static let black = UIColor(hexCode: "#000000")
+    static let white = UIColor(hexCode: "#FFFFFF")
+    static let red = UIColor(hexCode: "#F04452")
+    static let blue = UIColor(hexCode: "#3282F8")
+    static let purple = UIColor(hexCode: "#914CF5")
+    static let gray = UIColor(hexCode: "#828282")
+    static let darkGray = UIColor(hexCode: "#343D4C")
+    static let lightGray = UIColor(hexCode: "#F3F4F6")
+    static let babyPink = UIColor(hexCode: "#FFEAED")
+    static let skyBlue = UIColor(hexCode: "#E5F0FF")
+}
---
+++ BitBox/DesignSystem/ImageStyle.swift
@@ -0,0 +1,21 @@
+//
+//  ImageStyle.swift
+//  BitBox
+//
+//  Created by Jaehui Yu on 2/28/24.
+//
+
+import UIKit
+
+enum ImageStyle {
+    static let starFill: UIImage = .btnStarFill
+    static let star: UIImage = .btnStar
+    static let favorite: UIImage = .tabFavorite
+    static let favoriteInactive: UIImage = .tabFavoriteInactive
+    static let search: UIImage = .tabSearch
+    static let searchInactive: UIImage = .tabSearchInactive
+    static let trend: UIImage = .tabTrend
+    static let trendInactive: UIImage = .tabTrendInactive
+    static let user: UIImage = .tabUser
+    static let userInactive: UIImage = .tabUserInactive
+}
