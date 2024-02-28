0e3a87255460d3acc66b7df7ab9de34338157a82
Jaehui Yu <pkj0522@naver.com>
2024-02-28 11:09:08 +0900

    feat: Add FontStyle for design system

---
+++ BitBox/DesignSystem/FontStyle.swift
@@ -0,0 +1,20 @@
+//
+//  FontStyle.swift
+//  BitBox
+//
+//  Created by Jaehui Yu on 2/28/24.
+//
+
+import UIKit
+
+enum FontStyle {
+    static let largeTitle: UIFont = .boldSystemFont(ofSize: 34)
+    static let semiLargeTitle: UIFont = .boldSystemFont(ofSize: 25)
+    static let title: UIFont = .systemFont(ofSize: 20, weight: .heavy)
+    static let semiTitle: UIFont = .systemFont(ofSize: 20, weight: .regular)
+    
+    static let primaryBold: UIFont = .boldSystemFont(ofSize: 17)
+    static let primary: UIFont = .systemFont(ofSize: 17)
+    static let secondaryBold: UIFont = .boldSystemFont(ofSize: 14)
+    static let secondary: UIFont = .systemFont(ofSize: 14)
+}
