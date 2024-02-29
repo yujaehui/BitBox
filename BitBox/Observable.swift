e59ed06849e2ebf9260d5f95c1c9fa8e91702a3e
Jaehui Yu <pkj0522@naver.com>
2024-02-29 21:02:22 +0900

    feat: Implement custom Observable for data binding

---
+++ BitBox/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  BitBox
+//
+//  Created by Jaehui Yu on 2/29/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    private var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ closure: @escaping (T) -> Void) {
+        closure(value)
+        self.closure = closure
+    }
+}
