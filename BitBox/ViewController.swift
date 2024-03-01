bbe0abb5fdd659eb64dcba66c56501ef0403c1e4
Jaehui Yu <pkj0522@naver.com>
2024-03-01 18:06:41 +0900

    feat: Add TabBarController with main app navigation

--- BitBox/ViewController.swift
+++ BitBox/ViewController.swift
@@ -2,18 +2,33 @@
 //  ViewController.swift
 //  BitBox
 //
-//  Created by Jaehui Yu on 2/7/25.
+//  Created by Jaehui Yu on 3/1/24.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = ColorStyle.white
+        self.tabBar.tintColor = ColorStyle.purple
+        addVC()
     }
 
-
+    private func addVC() {
+        let trendVC = UINavigationController(rootViewController: TrendViewController())
+        trendVC.tabBarItem = UITabBarItem(title: "trend", image: ImageStyle.trendInactive, selectedImage: ImageStyle.trend)
+        
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        searchVC.tabBarItem = UITabBarItem(title: "search", image: ImageStyle.searchInactive, selectedImage: ImageStyle.search)
+        
+        let favoriteVC = UINavigationController(rootViewController: FavoriteViewController())
+        favoriteVC.tabBarItem = UITabBarItem(title: "favorite", image: ImageStyle.favoriteInactive, selectedImage: ImageStyle.favorite)
+        
+        let userVC = UINavigationController(rootViewController: UserViewController())
+        userVC.tabBarItem = UITabBarItem(title: "user", image: ImageStyle.userInactive, selectedImage: ImageStyle.user)
+        
+        self.viewControllers = [trendVC, searchVC, favoriteVC, userVC]
+    }
 }
-
